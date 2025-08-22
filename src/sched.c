d510d37131b7f8d28f862eaaad6c86fb0e74d322
Jattie van der Linde <jattie@vanderlindes.net>
2025-08-22 12:38:08 +0100

    edits required to allow make to run without errors by omitting all references to prtouch

--- src/sched.c
+++ src/sched.c
@@ -260,8 +260,8 @@ run_tasks(void)
                     irq_wait();
 #else
 					asm volatile("cpsie i" ::: "memory");
-					extern void prtouch_task(void);
-					prtouch_task();
+//					extern void prtouch_task(void);
+//					prtouch_task();
 #endif
                 } while (SchedStatus.tasks_status != TS_REQUESTED);
             }
